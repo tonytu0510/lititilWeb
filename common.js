57aa54448545ae0f2896e10476718f25ba0c3872
dalei <tuhonglei@yeah.net>
2026-07-17 11:43:10 +0800

    自动发布更新

--- common.js
+++ common.js
@@ -24,9 +24,7 @@
     <button class="close-btn" onclick="toggleDino()" style="position:absolute;left: 15px;top: calc(50% - 3px);transform:translateY(-50%);z-index:10;background:none;border:none;color:#fff;cursor:pointer;font-size:14px;">✕</button>
     
     <!-- 开始游戏按钮（最右边） -->
-<!--
     <button id="startGameBtn" onclick="startDinoGame()" style="position:absolute;right:90px;top:50%;transform:translateY(-50%);z-index:10;background:#fff;color:#c4334c;border:none;padding:4px 12px;border-radius:4px;cursor:pointer;font-size:12px;font-weight:bold;">开始游戏</button>
--->
     
     <!-- 问号图标 -->
     <button id="helpBtn" onclick="showHelp()" style="position:absolute;right:40px;top:50%;transform:translateY(-50%);z-index:10;background:#fff;color:#c4334c;border:none;width:22px;height:22px;border-radius:50%;cursor:pointer;font-size:14px;font-weight:bold;">?</button>
