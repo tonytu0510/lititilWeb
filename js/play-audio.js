df93ae41355ea482c55f2bf1421e45af3fe85bc0
dalei <tuhonglei@yeah.net>
2026-08-08 18:28:19 +0800

    自动发布更新

--- js/play-audio.js
+++ js/play-audio.js
@@ -10,13 +10,10 @@
 
     function autoPlayOnClick() {
         play();
-        document.addEventListener("load", autoPlayOnClick);
         document.removeEventListener('click', autoPlayOnClick);
         document.removeEventListener('touchstart', autoPlayOnClick);
     }
-    document.addEventListener("load", autoPlayOnClick);
-    document.addEventListener('click', autoPlayOnClick);
-    document.addEventListener('touchstart', autoPlayOnClick);
+    autoPlayOnClick()
 
     window.playAudio = play;
 })();
