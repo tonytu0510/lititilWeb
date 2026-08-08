c3b64ea44f39ebed205805914164ae283a536883
dalei <tuhonglei@yeah.net>
2026-08-08 23:30:34 +0800

    自动发布更新

--- js/play-audio.js
+++ js/play-audio.js
@@ -1,6 +1,6 @@
 // play-audio.js
 (function() {
-    let flag = false
+    localStorage.setItem('playFlag', '0')
     let audio = null;
     function play() {
         if (audio) { audio.pause(); audio.currentTime = 0; }
@@ -12,12 +12,20 @@
         play();
         document.removeEventListener('click', autoPlayOnClick);
         document.removeEventListener('touchstart', autoPlayOnClick);
-        flag = true
+        localStorage.setItem('playFlag', '1')
     }
-    if(!flag) {
+    let getPlayFlag = localStorage.getItem('playFlag')
+    if(getPlayFlag === '0') {
         document.addEventListener('click', autoPlayOnClick);
         document.addEventListener('touchstart', autoPlayOnClick);
     }
+    window.addEventListener('beforeunload', function (e) {
+        // 取消事件的默认行为
+        e.preventDefault();
+        // Chrome 需要返回非空字符串才会显示确认对话框
+        localStorage.setItem('playFlag', '0')
+    });
+
 
     window.playAudio = play;
 })();
