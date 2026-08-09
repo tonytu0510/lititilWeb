38d6fd24e9e66d8e6808b7efc1fe5cbd30ef7287
dalei <tuhonglei@yeah.net>
2026-08-09 12:59:34 +0800

    自动发布更新

--- js/play-audio.js
+++ js/play-audio.js
@@ -1,26 +1,45 @@
 // play-audio.js
 (function() {
     let audio = null;
+    let isBound = false;
     let flag = getUrlParams().playFlag || null
     function play() {
         if (audio) { audio.pause(); audio.currentTime = 0; }
         audio = new Audio('audio/people-long-live.mp3');
         audio.play();
     }
+    function bindClick() {
+        if (isBound) return;
+        isBound = true;
+        document.addEventListener('click', autoPlayOnClick);
+        document.addEventListener('touchstart', autoPlayOnClick);
+    }
 
     function autoPlayOnClick() {
         play();
         document.removeEventListener('click', autoPlayOnClick);
         document.removeEventListener('touchstart', autoPlayOnClick);
+        isBound = false;
     }
-    let getPlayFlag = !!!flag
-    if(getPlayFlag) {
-        document.addEventListener('click', autoPlayOnClick);
-        document.addEventListener('touchstart', autoPlayOnClick);
+
+    function autoPlay() {
+        let getPlayFlag = !!!flag;
+        if (getPlayFlag) {
+            bindClick();
+        }
+        document.addEventListener('keydown', keyHandler);
     }
+
+    autoPlay()
 
     window.playAudio = play;
 
+    function keyHandler(e) {
+        if (e.code === 'KeyM') {
+            e.preventDefault();
+            play(); // 直接播放
+        }
+    }
     function getUrlParams(url) {
         const params = {};
         const queryString = url ? url.split('?')[1] : window.location.search.slice(1);
