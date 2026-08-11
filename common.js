b0d433245f3c4581e138586e0c7d13a5986e5d25
dalei <tuhonglei@yeah.net>
2026-08-11 23:48:07 +0800

    自动发布更新

--- common.js
+++ common.js
@@ -13,7 +13,7 @@
 	                <a href="qrCode.html">二维码</a>
 	                <a href="nestedInvolutionRingHuge.html">对合环</a>
 	                <a href="chinaClock.html">RSTUV</a>
-	                <a href="goodMe.html">GoodMe</a>
+	                <!--<a href="goodMe.html">GoodMe</a>-->
 	                <a href="namePairing.html">缘分测试</a>
                     <a href="aboutMe.html">关于我</a>
                 </nav>
