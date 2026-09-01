8babdd9be6cb50507fa261c55b00d6abce5ddc98
dalei <tuhonglei@yeah.net>
2026-09-02 02:58:53 +0800

    自动发布更新

--- common.js
+++ common.js
@@ -65,6 +65,7 @@
                     <a href="panding.html">判证</a>
                     <a href="130demo.html">130 演示</a>
                     <a href="notes.html">笔记</a>
+                    <a href="cable.html">布线笔记</a>
                     <a href="aboutMe.html">关于我</a>
                 </nav>
             </div>
