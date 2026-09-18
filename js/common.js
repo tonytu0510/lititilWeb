af6754b2627d08d05c74fec2297d3c4d5fe0fb85
dalei <tuhonglei@yeah.net>
2026-09-18 23:36:05 +0800

    自动发布更新

--- js/common.js
+++ js/common.js
@@ -856,7 +856,7 @@
             @media (max-width: 500px) {
                 .scroll-nav {
                     right: 20px;
-                    width: 42px;
+                    width: 34px;
                 }
                 .scroll-nav-btn {
                     height: 50px;
