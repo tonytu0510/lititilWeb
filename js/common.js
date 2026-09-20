479cac18baba7ab03cde7aed326d6f6e4692f976
dalei <tuhonglei@yeah.net>
2026-09-20 14:25:02 +0800

    自动发布更新

--- js/common.js
+++ js/common.js
@@ -75,7 +75,8 @@
             { name: '判证',     href: 'panding.html' },
             { name: '读呼吸',   href: 'BreathBetweenWords.html' },
             { name: '想法',     href: 'idea.html' },
-            { name: '关于我',   href: 'aboutMe.html' }
+            { name: '关于我',   href: 'aboutMe.html' },
+            { name: '灵感来源', href: 'renming.html' }
         ]
     };
 
